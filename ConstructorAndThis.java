1d64792a51d3966fbb8245e6e55ddd91229260f6
Muhammad Abdul Hadi Khawaja <88159328+Abdulhadi56@users.noreply.github.com>
2025-10-23 15:35:34 +0500

    Add files via upload

---
+++ ConstructorAndThis.java
@@ -0,0 +1,21 @@
+/*
+
+// --- 'this' keyword and Constructor of the class in OOP
+class Constructor {
+    int x;
+    public Constructor(int x){
+        this.x = x;    
+    }
+    
+    public int sum(int a, int b){
+        int c = a + b;
+        return c;
+    }
+    
+	public static void main(String[] args) {
+		Constructor obj = new Constructor(6);
+		System.out.println(obj.x);
+		System.out.println("The value of c: " + obj.sum(2, 3));
+	}
+}
+*/
