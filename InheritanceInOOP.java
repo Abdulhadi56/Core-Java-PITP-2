3a8bdfab78b9f27fa5246eeec548745995ffe813
Muhammad Abdul Hadi Khawaja <88159328+Abdulhadi56@users.noreply.github.com>
2025-10-23 15:35:58 +0500

    Add files via upload

---
+++ InheritanceInOOP.java
@@ -0,0 +1,29 @@
+
+//CORE JAVA TASKS --- Inheritance in OOP, using the 'extends' keyword
+
+class Parent{
+    protected void greetings(){
+        System.out.println("Hello there..");
+    }
+}
+
+class Child extends Parent{
+    public void displayInfo(){
+        System.out.println("Hello..");
+    }
+    
+    /*public static void main(String[] args){
+        Child obj = new Child();
+        obj.greetings();
+        obj.displayInfo();
+    }*/
+}
+
+public class Main{
+    public static void main(String[] args){
+        //Main obj = new Main();
+        Child obj2 = new Child();
+        obj2.greetings();
+        obj2.displayInfo()= 
+    }
+}
