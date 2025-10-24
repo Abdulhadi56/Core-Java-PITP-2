719afb211f6b742aff35805790694580e488da3d
Muhammad Abdul Hadi Khawaja <88159328+Abdulhadi56@users.noreply.github.com>
2025-10-24 23:08:29 +0500

    Add files via upload

---
+++ abstractionInOOP.java
@@ -0,0 +1,74 @@
+abstract class Payment {
+    double amount;
+
+    Payment(double amount) {
+        this.amount = amount;
+    }
+
+    // Abstract method - har subclass apni implementation degi
+    abstract void pay();
+
+    // Normal method - sab me same kaam karega
+    void paymentReceipt() {
+        System.out.println("Payment of $" + amount + " successful. Receipt generated.");
+    }
+}
+
+// CreditCard payment
+class CreditCardPayment extends Payment {
+    CreditCardPayment(double amount) {
+        super(amount);
+    }
+
+    @Override
+    void pay() {
+        System.out.println("Paid $" + amount + " using Credit Card");
+    }
+}
+
+// PayPal payment
+class PayPalPayment extends Payment {
+    PayPalPayment(double amount) {
+        super(amount);
+    }
+
+    @Override
+    void pay() {
+        System.out.println("Paid $" + amount + " using PayPal");
+    }
+}
+
+// Bank Transfer payment
+class BankTransferPayment extends Payment {
+    BankTransferPayment(double amount) {
+        super(amount);
+    }
+
+    @Override
+    void pay() {
+        System.out.println("Paid $" + amount + " using Bank Transfer");
+    }
+}
+
+// Main class
+public class abstractClass {
+    public static void main(String[] args) {
+        Payment p1 = new CreditCardPayment(100);
+        Payment p2 = new PayPalPayment(250);
+        Payment p3 = new BankTransferPayment(500);
+
+        // Polymorphism: same reference type, different behaviors
+        p1.pay();
+        p1.paymentReceipt();
+
+        System.out.println();
+
+        p2.pay();
+        p2.paymentReceipt();
+
+        System.out.println();
+
+        p3.pay();
+        p3.paymentReceipt();
+    }
+}
